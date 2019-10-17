f5fc90162ed271282c7f85b06da9bd82f77499cb
magnuswenzer <magnus@wenzer.se>
2019-10-17 10:58:17 +0200

    requirements

--- setup.py
+++ setup.py
@@ -2,6 +2,12 @@ from setuptools import setup
 
 from sharkpylib import __version__
 
+requirements = []
+with open('requirements.txt') as fid:
+    for line in fid:
+        if not line.startswith('#'):
+            requirements.append(line.strip())
+
 setup(name='sharkpylib',
     version=__version__,
     description='Python 3 library',
@@ -10,5 +16,5 @@ setup(name='sharkpylib',
     author_email='shark@smhi.se',
     license='MIT',
     packages=['sharkpylib'],
-    install_requires=[],
+    install_requires=requirements,
     zip_safe=False)
