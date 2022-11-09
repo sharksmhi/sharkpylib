08df81582977f17c64d15dc7929a9f0e2a67056c
magnuswenzerSMHI <magnus.wenzer@smhi.se>
2022-11-09 08:58:24 +0100

    Handle / in station names

--- sharkpylib/plot/__init__.py
+++ sharkpylib/plot/__init__.py
@@ -7,8 +7,9 @@ def create_seabird_like_plots_for_package(pack, directory, suffix='png', **kwarg
     img_paths = []
     config_files_obj = config.ConfigFiles()
     for conf_file in config_files_obj.iter_config_files_for_position(pack.lat_dd, pack.lon_dd):
-        file_path = Path(directory, f'{pack.key}_{pack("station", pref_suffix=".hdr")}_{conf_file.name}'
-                                    f'.{suffix.strip(".")}')
+        file_name = f'{pack.key}_{pack("station", pref_suffix=".hdr")}_{conf_file.name}.{suffix.strip(".")}'.replace(
+            '/', '-')
+        file_path = Path(directory, file_name)
         plot = ProfilePlot4(pack)
         plot.plot_from_config(conf_file.get_config(**kwargs))
         path = plot.save(file_path)
